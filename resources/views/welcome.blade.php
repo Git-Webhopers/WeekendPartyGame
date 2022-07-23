737cba4abf3e1c6337cdb08f4e1d54f0b7bab796
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-07-24 00:01:00 +0530

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
 <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
@@ -31,12 +31,14 @@
                     <x-global.logo class="w-auto h-40 mx-auto text-indigo-600" />
                 </a>
 
-                <h1 class="text-5xl font-extrabold tracking-wider text-center text-gray-600">
-                    {{ config('app.name') }}
+                <h1 class="text-5xl font-extrabold tracking-wider text-center">
+                    <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-purple-500">
+                        {{ config('app.name') }}
+                    </span>
                 </h1>
 
                 <div>
-                    <button type="button" class="text-white font-bold bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-lg px-40 py-4 text-center flex">
+                    <button type="button" class="text-white hover:bg-gradient-to-l font-bold bg-gradient-to-br from-pink-500 via-purple-600 to-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-lg px-40 py-4 text-center flex">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
