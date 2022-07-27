2fe3bb515f00c1683bbaf6c2f74c05cd6d01b762
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-07-27 22:00:19 +0530

    Game Model Created

---
+++ database/migrations/2022_07_27_161421_create_games_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('looser_id')->nullable()->references('id')->on('users');
+            $table->enum('status', ['To Be Started', 'Being Played', 'Game Over', 'Cancelled'])->default('To Be Started');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
